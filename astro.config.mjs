9cc4f5a75e78badb0c251b8c5fc66603d8c488bd
sekikawa-cpu <sekikawa0301@gmail.com>
2026-04-24 23:40:01 +0900

    Fix: Properly configure Tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  // サイトマップ機能を一旦削除しました
-  integrations: [],
+  integrations: [tailwind()],
 });
