30c18974b17de2e45acfa86d0f235088d8560c1a
sekikawa-cpu <sekikawa0301@gmail.com>
2026-04-26 16:57:26 +0900

    fix: add site URL for sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://astro-investment-blog-1gwd.vercel.app',
   integrations: [
@@ -10,3 +9,4 @@ export default defineConfig({
     sitemap(),
   ],
 });
+
