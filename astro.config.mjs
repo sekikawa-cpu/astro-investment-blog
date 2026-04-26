bd18fe0a2b35db418807f294aa0e9b83f308b9f7
sekikawa-cpu <sekikawa0301@gmail.com>
2026-04-26 23:47:01 +0900

    fix: disable sitemap temporarily to fix build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://astro-investment-blog-1gwd.vercel.app',
   integrations: [
     tailwind(),
-    sitemap(),
   ],
 });
 
