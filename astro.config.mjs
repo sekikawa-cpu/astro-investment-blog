98707eef14a0d479981ea67b5edf76206729802d
sekikawa-cpu <sekikawa0301@gmail.com>
2026-04-29 23:07:45 +0900

    大幅な修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  image: {
+    domains: ['cover.openbd.jp', 'books.google.com'],
+  },
   site: 'https://astro-investment-blog-1gwd.vercel.app',
   integrations: [
     tailwind(),
