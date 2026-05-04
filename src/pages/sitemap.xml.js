c4b23fb0ccb06c9392b4161c39b8126e7e8e3b83
sekikawa-cpu <sekikawa0301@gmail.com>
2026-05-04 21:53:33 +0900

    feat: Add dynamic sitemap.xml generation for SEO

---
+++ src/pages/sitemap.xml.js
@@ -0,0 +1,51 @@
+import { getCollection } from 'astro:content';
+
+export async function GET() {
+  const posts = await getCollection('blog');
+  const site = 'https://www.tech-wealth-mind.com';
+
+  const staticPages = [
+    '',
+    '/profile',
+    '/contact',
+    '/privacy',
+    '/search',
+  ];
+
+  const allPages = [
+    ...staticPages.map(page => ({
+      url: `${site}${page}`,
+      lastmod: new Date().toISOString().split('T')[0],
+      changefreq: 'weekly',
+      priority: page === '' ? '1.0' : '0.8',
+    })),
+    ...posts.map(post => ({
+      url: `${site}/blog/${post.slug}`,
+      lastmod: post.data.updatedDate
+        ? post.data.updatedDate.toISOString().split('T')[0]
+        : post.data.pubDate.toISOString().split('T')[0],
+      changefreq: 'monthly',
+      priority: '0.7',
+    })),
+  ];
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${allPages
+  .map(
+    page => `  <url>
+    <loc>${page.url}</loc>
+    <lastmod>${page.lastmod}</lastmod>
+    <changefreq>${page.changefreq}</changefreq>
+    <priority>${page.priority}</priority>
+  </url>`
+  )
+  .join('\n')}
+</urlset>`;
+
+  return new Response(xml, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+}
